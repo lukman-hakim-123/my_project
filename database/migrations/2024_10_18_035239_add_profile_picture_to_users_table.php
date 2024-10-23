a0ee21b2273cabc873ddd67385c9061b3a6c57c9
lukman-hakim-123 <lh146244@gmail.com>
2024-10-23 03:26:58 +0000

    add profile success

--- database/migrations/2024_10_18_035239_add_profile_picture_to_users_table.php
+++ database/migrations/2024_10_18_035239_add_profile_picture_to_users_table.php
@@ -23,8 +23,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             
-                $table->dropColumn('profile_picture');
-            
+          
         });
     }
 };
